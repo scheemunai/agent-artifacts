bbe9114f9c896638793b92324540167d7af16c79
CRHQ <noreply@crhq.ai>
2026-08-28 15:58:11 +0000

    Fix new bot disclosure width
    Co-Authored-By: CRHQ <noreply@crhq.ai>

--- src/ui/pages/dashboard.tsx
+++ src/ui/pages/dashboard.tsx
@@ -1347,7 +1347,7 @@ function NewBotDisclosure({
               optional
             />
             <Button variant="primary" type="submit">
-              New bot
+              Create bot
             </Button>
           </form>
         </Card>
